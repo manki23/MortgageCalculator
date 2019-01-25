23e9e72691bce221aa719c610ef9947b70e7f6c5
IreneZhou0129 <xiaoxinzhou0@gmail.com>
2019-01-24 20:05:58 -0500

    Add Setting and Details pages.Fix the error!

---
+++ app/src/main/java/com/uottawa/mortgagecalculator/activity_details_page.java
@@ -0,0 +1,13 @@
+package com.uottawa.mortgagecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_details_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_page);
+    }
+}
---
+++ app/src/main/java/com/uottawa/mortgagecalculator/activity_setting_page.java
@@ -0,0 +1,19 @@
+package com.uottawa.mortgagecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+public class activity_setting_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_page);
+    }
+}
+
+
+
+
