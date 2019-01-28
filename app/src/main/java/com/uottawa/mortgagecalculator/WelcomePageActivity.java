b92a116b03f2eb7c5b1de89730af354e7c090275
manki23 <manki@student.42.fr>
2019-01-28 15:28:10 -0500

    Welcome Page v1

--- app/src/main/java/com/uottawa/mortgagecalculator/WelcomePageActivity.java
+++ app/src/main/java/com/uottawa/mortgagecalculator/WelcomePageActivity.java
@@ -1,13 +1,49 @@
 package com.uottawa.mortgagecalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class WelcomePageActivity extends AppCompatActivity {
+public class WelcomePageActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
+        Spinner spinner = findViewById(R.id.interestSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.interestRates, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        Spinner spinner2 = findViewById(R.id.yearSpinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.years, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void calculBtn_onClick(View view) {
+        Intent gotoSummary = new Intent(WelcomePageActivity.this, SummaryPageActivity.class);
+        //gotoSummary.putExtra()
+        startActivity(gotoSummary);
     }
 }
